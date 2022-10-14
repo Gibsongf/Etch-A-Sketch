a461c81b6b649c1d888d3a97f5487f54835871cf
Gibsongf <maccgiiibson@gmail.com>
2022-10-14 15:30:38 -0300

    finished but need more css and remove the need to hold space.

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
 # Etch-A-Sketch
-
-preciso remover os quadrados toda a vez que o numero for alterado no slide
-
-eles funcionam ao clicar no start (fazer os novos quadrados funcionar como antes)
-
-mostrar o numero de quadrados no slides
-
-adicionar seleçao de cores e modo de cores aleatorias e linkar a tecla espaço com o pause na tela de desenho
-
-mais css style nos botoes e na barra de seleçoes ao lado
--- script.js
+++ script.js
@@ -2,17 +2,31 @@
 
 
 function changeColor(){
-    this.setAttribute('style','background: red;');
+    
+    let r = String(Math.floor(Math.random() * 255));
+    let g = String(Math.floor(Math.random() * 255));
+    let b = String(Math.floor(Math.random() * 255));
+    if (randomColor=== true){
+        console.log('random')
+        this.setAttribute('style',`background-color:rgb(${r},${g},${b});`);
+    }
+    else{
+        
+        console.log(selectColorPicker.value)
+        this.setAttribute('style',`background-color:${selectColorPicker.value};`);
+
+    }
     
 }
 function toggleSetting(){
     const grids = document.querySelectorAll('.grid');
-
+    
     if (this.textContent == 'Start'){
         grids.forEach(grid => grid.addEventListener("mouseover",changeColor));
-        this.setAttribute('style','border-color: Blue;')
-        btnStop.setAttribute('style','border-color: None;')
-        btnReset.setAttribute('style','border-color: None;')
+        this.setAttribute('style','border-color: Blue;');
+        btnStop.setAttribute('style','border-color: None;');
+        btnReset.setAttribute('style','border-color: None;');
+        return;
 
     }
     if (this.textContent == 'Reset'){
@@ -20,62 +34,72 @@ function toggleSetting(){
             grid.setAttribute('style','background: white;')));
         grids.forEach(grid => grid.removeEventListener("mouseover",changeColor));      
         
-        this.setAttribute('style','border-color: Blue;')
-        btnStart.setAttribute('style','border-color: None;')
-        btnStop.setAttribute('style','border-color: None;')
-
+        this.setAttribute('style','border-color: Blue;');
+        btnStart.setAttribute('style','border-color: None;');
+        btnStop.setAttribute('style','border-color: None;');
+        return;
 
     }
 
     if (this.textContent == 'Stop'){
         grids.forEach(grid => grid.removeEventListener("mouseover",changeColor));
-        btnStart.setAttribute('style','border-color: None;')
-        btnReset.setAttribute('style','border-color: None;')
-        this.setAttribute('style','border-color: Blue;')
+        btnStart.setAttribute('style','border-color: None;');
+        btnReset.setAttribute('style','border-color: None;');
+        this.setAttribute('style','border-color: Blue;');
+        return;
     }
     
 }
 function makeGrid(nxn = 32){
+    btnStart.setAttribute('style','border-color: None;');  
     let containerLength = container.children.length;
+   
     
     if (containerLength <= 1){
-        mGrid(32)
-        return
+        mGridSpace(32);      
+        return;
     }
 
     if (nxn == containerLength || nxn == gridContainer.children.length){  
-        return
+        return;
     }
     if(slider.value === "0"){
         nxn = 16;
+        output.innerHTML = `16 x 16`;
     }
-    if(slider.value === "100"){
-        nxn = 62; 
-    }
+
     if(slider.value === "50"){
-        nxn = 32; 
+        nxn = 32;    
+        output.innerHTML = `32 x 32`;
+    }
+    
+    if(slider.value === "100"){
+        nxn = 64; 
+        output.innerHTML = `64 x 64`;
     }
     
     if (nxn == containerLength && nxn == gridContainer.children.length){
-        console.log('igual test')
-        return
+      
+        return;
     }
+
     if (nxn > containerLength && nxn > gridContainer.children.length){
-        console.log('TESTE IF ')
-        mGrid(nxn)
+
+        mGridSpace(nxn);    
     }
     if (nxn < containerLength && nxn < gridContainer.children.length){
-        console.log('TESTE IF NXN MENOR N')
-        mGrid(nxn)
+
+        mGridSpace(nxn);
+        
     }
-    /* console.log("nxn , containerLen   , gridContainer.len") */
+    
     
   
     
     
 }
 
-function mGrid(nxn){
+function mGridSpace(nxn){
     if (container.children.length > 1){
         while (container.firstChild) {
             container.removeChild(container.lastChild);
@@ -83,46 +107,71 @@ function mGrid(nxn){
         while (gridContainer.firstChild) {
             gridContainer.removeChild(gridContainer.lastChild);
         }
-        /* console.log('REMOVE')
-        console.log(container.children.length, gridContainer.children.length) */
+ 
     }
     for (let i = 1; i <= nxn; i++){
         
         const grid = document.createElement('div');
         grid.classList.add('grid');
-        /* grid.textContent = "i" */
+
         gridContainer.appendChild(grid);
         
     };
-    //one number less than the needed here
     for (let i = 1; i <= nxn ; i++){
         if (container.children.length === gridContainer.children.length){
-            break
+            break;
         }
         const clone = gridContainer.cloneNode(true);
         container.appendChild(clone);    
     };
-    console.log(container.children.length, gridContainer.children.length)
+
 
 }
 
 
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+var slider = document.getElementById("Slider");
+var output = document.getElementById("grid-item-size");
+output.innerHTML = `32 x 32`;
 
 const container = document.querySelector('.container');
 const gridContainer = document.querySelector('.grid-container');
 
 const btnStart = document.querySelector('#Start');
+const btnRandomColors = document.querySelector('#Random-Colors')
+const selectColorPicker = document.querySelector('#colorpicker')
 const btnReset = document.querySelector('#Reset');
 const btnStop = document.querySelector('#Stop');
+const body = document.querySelector('body');
+let randomColor = false;
+btnStop.addEventListener('click',toggleSetting);
+body.addEventListener('keydown',function(event) {
+    if (event.keyCode == 32) {
+        btnStop.click();
+        
+  
+}})
 
-btnStop.addEventListener('click',toggleSetting)
-btnReset.addEventListener('click',toggleSetting)
-btnStart.addEventListener('click',toggleSetting)
+let selectedColor = selectColorPicker.value;
+
+btnReset.addEventListener('click',toggleSetting);
+btnStart.addEventListener('click',toggleSetting);
+btnRandomColors.addEventListener('click',function(){
+    if(randomColor=== false){
+        this.setAttribute('style','border-color: Blue;');
+        randomColor = true;
+        return;
+    }
+    if(randomColor=== true){
+        this.setAttribute('style','border-color: none;');
+        randomColor = false;
+        return;
+    }
+    });
+
+slider.addEventListener('input',makeGrid);
 slider.addEventListener('click',makeGrid);
 
-makeGrid()
 
+makeGrid();
 
